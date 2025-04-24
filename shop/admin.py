664648ee53566b122c98bbc66d3740e6d595d641
jaysonar092 <jaysonar092@gmail.com>
2025-04-24 23:29:27 +0530

    restric products model edit to admin only

--- shop/admin.py
+++ shop/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Category, Product
 
 admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'available')
+    list_filter = ('available', 'category')
+    search_fields = ('name',)
+
+admin.site.register(Product, ProductAdmin)
