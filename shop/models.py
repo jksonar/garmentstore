b9554fc6111807bff6e9cc227f5df30047d9b7ea
jaysonar092 <jaysonar092@gmail.com>
2025-04-24 23:18:09 +0530

    applied styling to templates

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -17,8 +18,10 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
