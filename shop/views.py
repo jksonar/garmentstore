66c5548537e6e7c7adfce4a276d483f273f44d47
jay <jksonar1992@gmail.com>
2025-04-24 18:25:18 +0530

    init commit

---
+++ shop/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from django.shortcuts import redirect
+from .cart import Cart
+from django.views.decorators.http import require_POST
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product_list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    })
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, 'shop/product_detail.html', {
+        'product': product
+    })
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=1)
+    return redirect('shop:cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('shop:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'shop/cart_detail.html', {'cart': cart})
